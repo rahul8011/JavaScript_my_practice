928d00a4865f0061d5d5f56479ec22d247e42d88
Rahul Kumar <139959610+rahul8011@users.noreply.github.com>
2025-02-23 01:46:52 +0530

    Add files via upload

---
+++ class1/index1.js
@@ -0,0 +1,69 @@
+// this is comment
+//console.log('Hello jee kaise ho saare 1');
+//Dynamic Typing
+// let a = 5;
+// console.log(a);
+// a='I HATE YOU';
+// console.log(a);
+// a=true;
+// console.log(a);
+
+//Ternary operator
+// let age = 17;
+// let status = (age >= 18) ? 'I can vote' : 'I can not vote';
+// console.log(status);
+
+// let details = prompt('Enter the details married/not married');
+// let welcome = (details === 'married') ? 'Welcome to the party' : 'Sorry you are not allowed';
+// console.log(welcome);
+
+//control statement
+// 1. if-else
+// 2. Switch
+// let marks = prompt('Enter the marks here:');
+// if(marks >= 90)
+// {
+//     console.log(prompt('O'));
+// }
+// else if(marks >= 80)
+// {
+//     console.log(prompt('A'));
+// }
+// else if(marks >= 70)
+// {
+//     console.log(prompt('B'));
+// }
+// else if(marks >= 60)
+// {
+//     console.log(prompt('C'))
+// }
+// else (marks >= 50)
+// {
+//     console.log(prompt('S'));
+// }
+
+//2. Switch
+
+// 
+
+//LOOPS
+//1. FOR LOOP
+// for(let i=0; i<=10; i++)
+// {
+//     console.log(prompt(i));
+// }
+// 2.WHILE LOOP
+
+// let x=1;
+// while(x<=10)
+// {
+//     console.log(x);
+//     x++;
+// }
+//3.DO-WHILE-LOOP
+
+let num = 1;
+do{
+    console.log(num);
+    num++;
+}while(num<=20);
